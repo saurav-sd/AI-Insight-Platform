361b8e2340452205623df6b4501a540d499bcf23
saurav-sd <sauravdaware777@gmail.com>
2026-04-06 23:43:29 +0530

    feat: update environment configuration and script for database connection

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,13 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:admin123@localhost/analytics_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # create_engine → connects to database
 # SessionLocal → manages database transactions
 # Base → base class for models
 
-engine = create_engine(DATABASE_URL) # connects to the database
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"}) # connects to the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # manages database transactions
 Base = declarative_base() # base class for models, used to define database tables and their relationships
 
--- Backend/start.sh
+++ Backend/start.sh
@@ -1,1 +1,3 @@
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
+#!/bin/bash
+
+uvicorn app.main:app --host 0.0.0.0 --port 8000
